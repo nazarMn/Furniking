dc1e50884a06fdadfb9351403064384ec96617b5
MarkivNazar <69341802+nazarMn@users.noreply.github.com>
2024-08-24 16:04:02 +0300

    1

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const path = require('path');
+
+
+
+const PORT = process.env.PORT || PORT;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname, 'public', 'index.html')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server work on PORT: ${PORT}`);
+})
