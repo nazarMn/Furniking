ca97492f033be0ea84d97d73a5edd949db8c34be
MarkivNazar <69341802+nazarMn@users.noreply.github.com>
2024-08-24 16:14:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const path = require('path');
 
 
 
-const PORT = process.env.PORT || PORT;
+// const PORT = process.env.PORT || PORT;
+
+const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
